109b6ad11484990fd5d5a172202c0c0336c5c5c3
Project-Vigil-1602 <project.vigil.1602@gmail.com>
2024-11-06 07:56:09 +0530

    OK

--- src/pages/navbar.js
+++ src/pages/navbar.js
@@ -3,8 +3,8 @@ import React from "react";
 import { useHistory } from "react-router-dom";
 
 const pages = [
-  { title: "Summary", role: "manager", path: "/summary" },
-  { title: "Calendar", role: "manager", path: "/calendar" },
+  // { title: "Summary", role: "manager", path: "/summary" },
+  // { title: "Calendar", role: "manager", path: "/calendar" },
   { title: "Orders", role: "manager", path: "/orders" },
   { title: "Add Users", role: "sudo", path: "/sudo/users/add" },
 ];
@@ -12,35 +12,41 @@ const pages = [
 export const NavBar = ({ role, current }) => {
   const history = useHistory();
 
-  return (
-    <Box
-      display="flex"
-      justify-content="space-around"
-      align-items="center"
-      padding="20px"
-      background="--color-lightD2"
-    >
-      {pages
-        .filter((page) => page.role === role || role === "sudo")
-        .map(
-          (page) =>
-            page.title !== current && (
-              <Text
-                key={page.path}
-                onClick={() => history.push(page.path)}
-                cursor="pointer"
-                margin="0 10px"
-                font="--lead"
-                padding="10px"
-                border-radius="5px"
-                transition="background-color 0.3s"
-                hover-background="--color-light"
-                hover-color="--primary"
-              >
-                {page.title}
-              </Text>
-            )
-        )}
-    </Box>
-  );
+  const pages_content = pages
+    .filter((page) => page.role === role || role === "sudo")
+    .map((page) =>
+      page.title !== current ? (
+        <Text
+          key={page.path}
+          onClick={() => history.push(page.path)}
+          cursor="pointer"
+          margin="0 10px"
+          font="--lead"
+          padding="10px"
+          border-radius="5px"
+          transition="background-color 0.3s"
+          hover-background="--color-light"
+          hover-color="--primary"
+        >
+          {page.title}
+        </Text>
+      ) : null
+    )
+    .filter(Boolean); // Removes any `null` or `false` values from the array
+
+  if (pages_content.length > 0) {
+    return (
+      <Box
+        display="flex"
+        justify-content="space-around"
+        align-items="center"
+        padding="20px"
+        background="--color-lightD2"
+      >
+        {pages_content}
+      </Box>
+    );
+  }
+
+  return null;
 };
